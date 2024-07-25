9e288fe561c7e6723020e97c5f499653867286da
Alibehzad79 <Alibehzad13791122@gmail.com>
2024-07-25 21:47:21 +0330

    add drf_redesign third pkg

--- online_course/accounts_app/views.py
+++ online_course/accounts_app/views.py
@@ -4,16 +4,23 @@ from rest_framework.response import Response
 from rest_framework import status
 from accounts_app.serializers import RegisterSerializer, UserProfileSerializer
 from django.contrib.auth import get_user_model
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view, permission_classes, renderer_classes
 from rest_framework import permissions
 from django.contrib.auth.hashers import make_password
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.renderers  import BrowsableAPIRenderer, TemplateHTMLRenderer
 
 # Create your views here.
 
 
 @api_view(["POST"])
 def register_api_view(request):
+    """
+        email
+        password
+        first_name
+        last_name
+    """
     serializer = RegisterSerializer(data=request.data)
     if get_user_model().objects.filter(email=request.data["email"]).exists():
         return Response(
@@ -43,10 +50,11 @@ def user_profile_view(request):
     serializer = UserProfileSerializer(user, many=False)
     return Response(data=serializer.data, status=status.HTTP_200_OK)
 
-@api_view(['POST'])
+
+@api_view(["POST"])
 def logout(request):
     try:
-        refresh_token = request.data['refresh_token']
+        refresh_token = request.data["refresh_token"]
         token = RefreshToken(refresh_token)
         token.blacklist()
         return Response(status=status.HTTP_205_RESET_CONTENT)
